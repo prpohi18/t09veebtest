15923f04869158800459c95b7509ef08cd62aaca
janilv <ilves.jan@gmail.com>
2018-04-09 17:07:15 +0300

    upload

---
+++ Rahatest.java
@@ -0,0 +1,23 @@
+package ee.tlu.janilv.veebtest;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import org.junit.Test;
+
+public class Rahatest {
+    @Test
+    public void RahaTest(){
+        open("http://www.tlu.ee/~ttaevik/teisendus.html");
+       
+        
+        $("#lkvon").setValue("50");
+        $("#lkvonbtn").click();
+        $("#vastus").shouldHave(text("59.41615"));
+        
+        $("#pkvon").setValue("1");
+        $("#pkvonbtn").click();
+        $("#vastus").shouldHave(text("0.841523"));
+        
+        
+    }
+}
