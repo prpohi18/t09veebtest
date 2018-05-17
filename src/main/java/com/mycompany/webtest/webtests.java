92b09f0b08d6774e867c61b215a80d1caf07aa9e
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-05-17 11:13:40 +0300

    fix

--- src/main/java/com/mycompany/webtest/webtests.java
+++ src/main/java/com/mycompany/webtest/webtests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 
 public class webtests {
 	public static void main(String[] args){
+    open("http://localhost:63342/t09veebtest1/webtest/web.html?_ijt=o41tcvkmimhj29m8vuau09ol6m");
     $("#n1").setValue("5");
     $("#n2").setValue("3");
     $("#button").click();
