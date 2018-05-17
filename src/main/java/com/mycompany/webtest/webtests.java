7c80da28a64721f0356b2568b646abbf29a0b7ad
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-05-17 11:10:35 +0300

    tehtud

---
+++ src/main/java/com/mycompany/webtest/webtests.java
@@ -0,0 +1,23 @@
+package com.yourcompany.webtest;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import org.openqa.selenium.By;
+
+public class webtests {
+	public static void main(String[] args){
+    $("#n1").setValue("5");
+    $("#n2").setValue("3");
+    $("#button").click();
+    System.out.println("Vastus oli " + $("#answer").getText() + ", oodatav vastus: 15");
+
+    $("#n1").setValue("6");
+    $("#n2").setValue("9");
+    $("#button").click();
+    System.out.println("Vastus oli " + $("#answer").getText() + ", oodatav vastus: 54");
+
+    $("#n1").setValue("20");
+    $("#n2").setValue("25");
+    $("#button").click();
+    System.out.println("Vastus oli " + $("#answer").getText() + ", oodatav vastus: 500");
+	}
+}
